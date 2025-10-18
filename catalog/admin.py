f57970132d68fd2df3ee1e1620d9dcb1dd360ba3
Admin <ulb9@mail.ru>
2025-10-19 00:28:25 +0300

    созданы модели Product  и Category

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админ-панель для категорий"""
+    list_display = ('id', 'name', 'description')  # Что в списке
+    list_filter = ('name',)  # Фильтры справа
+    search_fields = ('name', 'description')  # Поля для поиска
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админ-панель для товаров"""
+    list_display = (
+        'id',
+        'name',
+        'category',
+        'price',
+        'created_at',
+        'updated_at'
+    )
+    list_filter = ('category', 'created_at', 'updated_at')
+    search_fields = ('name', 'description', 'category__name')
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'description', 'image', 'category')
+        }),
+        ('Финансовая информация', {
+            'fields': ('price',)
+        }),
+        ('Даты', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
